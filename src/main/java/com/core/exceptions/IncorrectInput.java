42d7d5adf97017f99488d775a1021378e40170ff
AnnaLevitskii <AnnaLevitskii@users.noreply.github.com>
2024-09-10 11:30:40 +0000

    deploy: 7e12cbc8b92c6d722634b9e3697ec6e600bddbf3

--- src/main/java/com/core/exceptions/IncorrectInput.java
+++
@@ -1,8 +0,0 @@
-package com.core.exceptions;
-
-public class IncorrectInput extends Exception{
-    public IncorrectInput(String exc){
-        super("Incorrect Input: \n" +
-                exc + " \n" );
-    }
-}
--- src/main/java/com/core/utils/Path.java
+++
@@ -1,5 +0,0 @@
-package com.core.utils;
-
-public interface Path {
-    String STORAGE_PATH = "src/main/java/com/core/utils/storage.csv";
-}
